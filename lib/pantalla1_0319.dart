53610774286323af0fbc81daa2a4a2cb3057b108
BeltranJ128 <143763139+BeltranJ128@users.noreply.github.com>
2024-03-13 09:23:09 -0600

    Update pantalla1_0319.dart

--- lib/pantalla1_0319.dart
+++ lib/pantalla1_0319.dart
@@ -8,21 +8,20 @@ class Pantalla1_0319 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Card p1 Beltrán 0319"),
+        title: Text("pantalla1 Beltrán 0319"),
         backgroundColor: Colors.pinkAccent,
       ),
       body: Center(
-        child: Card(
-          elevation: 10,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          color: Colors.indigoAccent,
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              "Card Beltrán",
-              style: TextStyle(fontSize: 30, color: Colors.white),
-            ),
+        child: Container(
+          color: Colors.blueAccent,
+          width: 300,
+          height: 300,
+          padding: EdgeInsets.all(32),
+          margin: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          alignment: Alignment.center,
+          child: Text(
+            "Card Beltrán",
+            style: TextStyle(fontSize: 40, color: Colors.white),
           ),
         ),
       ),
